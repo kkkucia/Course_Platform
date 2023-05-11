e67aac91d3e9be9195ff0f155efe8de5801b9783
kkkucia <11.karolinakucia@gmail.com>
2023-05-11 18:27:10 +0200

    Frontend init

---
+++ Frontend/course_platform/src/App.js
@@ -0,0 +1,13 @@
+import './Styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
