9a6b11c7886938604e55c51db7f2f3db3eae6956
Adam <Adam.ja.com@op.pl>
2023-05-15 22:08:08 +0200

    gitignore again?

--- Backend/src/main/java/org/example/controllers/courses/CoursesController.java
+++ Backend/src/main/java/org/example/controllers/courses/CoursesController.java
@@ -3,9 +3,13 @@ package org.example.controllers.courses;
 import org.example.controllers.MainController;
 import org.hibernate.Transaction;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.example.models.courses.Course;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CoursesController extends MainController {
@@ -17,8 +21,27 @@ public class CoursesController extends MainController {
     }
 
     @GetMapping("/courses/{courseName}")
-    public Object[] getCourse(@PathVariable("courseName") String courseName) {
-        return session.createQuery("from Course c where c.title=:courseName")
+    public List<Course> getCourse(@PathVariable("courseName") String courseName) {
+        Object[] queryResult = session.createQuery("from Course c where c.title=:courseName")
                 .setParameter("courseName", courseName).stream().toArray();
+        List<Course> courses = new ArrayList<>();
+        Course tmpCourse;
+        for (Object elem : queryResult) {
+            tmpCourse = (Course) elem;
+            courses.add(tmpCourse);
+        }
+        return courses;
+    }
+
+    @GetMapping("/courses")
+    public List<Course> getCourses() {
+        Object[] queryResult = session.createQuery("from Course").stream().toArray();
+        List<Course> courses = new ArrayList<>();
+        Course tmpCourse;
+        for (Object elem : queryResult) {
+            tmpCourse = (Course) elem;
+            courses.add(tmpCourse);
+        }
+        return courses;
     }
 }
