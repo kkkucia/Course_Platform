90ecdca4d63bbf65918c447b80f2a667cb90a4fa
Adam <Adam.ja.com@op.pl>
2023-06-10 19:25:04 +0200

    addCourseToCategory

--- Backend/src/main/java/org/example/controllers/courses/CategoriesController.java
+++ Backend/src/main/java/org/example/controllers/courses/CategoriesController.java
@@ -2,13 +2,16 @@ package org.example.controllers.courses;
 
 import org.example.controllers.MainController;
 import org.example.models.courses.Category;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class CategoriesController extends MainController {
@@ -19,5 +22,21 @@ public class CategoriesController extends MainController {
         List<Category> reservations = new ArrayList<>();
         Arrays.stream(queryResults).forEach(queryResult -> reservations.add((Category) queryResult));
         return reservations;
+    }
+
+    @CrossOrigin
+    @PostMapping("/categories/courses")
+    public ResponseEntity<HttpStatus> addCourseToCategory(@RequestBody Map<String, Long> json) {
+        try {
+            Query query = session.createSQLQuery(
+                            "CALL add_course_to_category(:course_id, :category_id)")
+                    .setParameter("course_id", json.get("course_id"))
+                        .setParameter("category_id", json.get("category_id"));
+            System.out.println(query.getResultList());
+        } catch (PersistenceException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(HttpStatus.BAD_REQUEST);
+        } catch (NegativeArraySizeException ignored) {
+        }
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 }
