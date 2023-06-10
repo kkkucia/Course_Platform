3d2219483d179e032c11ebe2bf8be5a9987e499b
Adam <Adam.ja.com@op.pl>
2023-06-10 20:21:32 +0200

    all mentors, specific mentor/participant

--- Backend/src/main/java/org/example/controllers/users/UsersController.java
+++ Backend/src/main/java/org/example/controllers/users/UsersController.java
@@ -1,14 +1,11 @@
 package org.example.controllers.users;
 
 import org.example.controllers.MainController;
+import org.example.models.users.Mentor;
 import org.example.models.users.Participant;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class UsersController extends MainController {
@@ -26,5 +23,40 @@ public class UsersController extends MainController {
         List<Participant> participants = new ArrayList<>();
         Arrays.stream(queryResults).forEach(queryResult -> participants.add((Participant) queryResult));
         return participants;
+    }
+
+    @CrossOrigin
+    @GetMapping("/participants/{participant_id}")
+    public Participant getParticipant(@PathVariable("participant_id") long participantId) {
+        Optional queryResults = session.createQuery("from Participant p where p.id = :participant_id")
+                .setParameter("participant_id", participantId)
+                .stream()
+                .findFirst();
+        if (queryResults.isPresent()) {
+            return (Participant) queryResults.get();
+        }
+        return null;
+    }
+
+    @CrossOrigin
+    @GetMapping("/mentors")
+    public List<Mentor> getMentors() {
+        Object[] queryResults = session.createQuery("from Mentor").stream().toArray();
+        List<Mentor> mentors = new ArrayList<>();
+        Arrays.stream(queryResults).forEach(queryResult -> mentors.add((Mentor) queryResult));
+        return mentors;
+    }
+
+    @CrossOrigin
+    @GetMapping("/mentors/{mentor_id}")
+    public Mentor getMentor(@PathVariable("mentor_id") long mentorId) {
+        Optional queryResults = session.createQuery("from Mentor m where m.id = :mentor_id")
+                .setParameter("mentor_id", mentorId)
+                .stream()
+                .findFirst();
+        if (queryResults.isPresent()) {
+            return (Mentor) queryResults.get();
+        }
+        return null;
     }
 }
